d5716d5f1785107f832ec2ebac017832350a7670
AbhirupDey12 <abhirupd470@gmail.com>
2023-02-04 12:21:01 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import React, { Component } from "react";
-import Form from "./component/Form";
-import LifecycleA from "./component/LifecycleA";
-import FragmentDemo from "./component/FragmentDemo";
-import Table from "./component/Table";
-import PureComp from "./component/PureComp";
-import ParentComp from "./component/ParentComp";
-import RefsDemo from "./component/RefsDemo";
-import Input from "./component/Input";
-import FocusInput from "./component/FocusInput";
-import FRParentInput from "./component/FRParentInput";
-import PortalDemo from "./component/PortalDemo";
-import Hero from "./component/Hero";
-import ErrorBoundary from "./component/ErrorBoundary";
-import ClickCounter from "./component/ClickCounter";
-import HoverCounter from "./component/HoverCounter";
-import ClickCounter2 from "./component/ClickCounter2";
-import HoverCounter2 from "./component/HoverCounter2";
-import User from "./component/User";
-import Counter from "./component/Counter";
-import ComponentC from "./component/ComponentC";
-import { UserProvider } from "./component/userContext";
-import PostList from "./component/PostList";
-import PostForm from "./component/PostForm";
+// import Form from "./component/Form";
+// import LifecycleA from "./component/LifecycleA";
+// import FragmentDemo from "./component/FragmentDemo";
+// import Table from "./component/Table";
+// import PureComp from "./component/PureComp";
+// import ParentComp from "./component/ParentComp";
+// import RefsDemo from "./component/RefsDemo";
+// import Input from "./component/Input";
+// import FocusInput from "./component/FocusInput";
+// import FRParentInput from "./component/FRParentInput";
+// import PortalDemo from "./component/PortalDemo";
+// import Hero from "./component/Hero";
+// import ErrorBoundary from "./component/ErrorBoundary";
+// import ClickCounter from "./component/ClickCounter";
+// import HoverCounter from "./component/HoverCounter";
+// import ClickCounter2 from "./component/ClickCounter2";
+// import HoverCounter2 from "./component/HoverCounter2";
+// import User from "./component/User";
+// import Counter from "./component/Counter";
+// import ComponentC from "./component/ComponentC";
+// import { UserProvider } from "./component/userContext";
+// import PostList from "./component/PostList";
+// import PostForm from "./component/PostForm";
 
+import React from "react";
+import ClassCounter from "./component/ClassCounter";
+import HookCounter from "./component/HookCounter";
+import HookCounter2 from "./component/HookCounter2";
+import HookCounterThree from "./component/HookCounterThree";
+import HookCounter4 from "./component/HookCounter4";
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
-  )
-} ;
+
+    <h1>Hello</h1>
+    
+    )
+};
 
 
-
+// <HookCounter4 />
+// <HookCounterThree />
+// <HookCounter />
+//  <HookCounter2 />
+// <ClassCounter />
 // let key = "2629cb07f801fedceda63abc5f4d1c51";
 // let city = 'London';
 
@@ -61,53 +72,53 @@ export default function App() {
 
 
 
-  //   cloudcover
-  // : 
-  // 9
-  // lifted_index
-  // : 
-  // 6
-  // prec_type
-  // : 
-  // "none"
-  // rh2m
-  // : 
-  // 7
-  // seeing
-  // : 
-  // 6
-  // temp2m
-  // : 
-  // 21
-  // timepoint
-  // : 
-  // 3
-  // transparency
-  // : 
-  // key={index} style={style} 
-  // Key	Name	Status	Actions
-  // 726289ec156b0e5c8e53ce64db4a1b2f
-  // https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
-  // 3
+//   cloudcover
+// : 
+// 9
+// lifted_index
+// : 
+// 6
+// prec_type
+// : 
+// "none"
+// rh2m
+// : 
+// 7
+// seeing
+// : 
+// 6
+// temp2m
+// : 
+// 21
+// timepoint
+// : 
+// 3
+// transparency
+// : 
+// key={index} style={style} 
+// Key	Name	Status	Actions
+// 726289ec156b0e5c8e53ce64db4a1b2f
+// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
+// 3
 
-  // const style = {
-  //   border: "1px solid black",
-  //   backgroundColor: "yellow",
-  //   marginTop: "10px"
-  // }
+// const style = {
+//   border: "1px solid black",
+//   backgroundColor: "yellow",
+//   marginTop: "10px"
+// }
 
-  // return (
-  //   <div>
+// return (
+//   <div>
 
-  //     <label htmlFor="CityName"> CityName </label>
-  //     <input type="text" id="CityName" value={city} onChange={temp} />
-  //     <button onClick={clickHandler} >Click Me</button>
+//     <label htmlFor="CityName"> CityName </label>
+//     <input type="text" id="CityName" value={city} onChange={temp} />
+//     <button onClick={clickHandler} >Click Me</button>
 
-      {/* {
+{/* {
         data.map((ele, index) => {
           return (
             <div > */}
-              {/* <p> Timepoint : {ele.timepoint} </p>
+{/* <p> Timepoint : {ele.timepoint} </p>
               <p> Cloudcover : {ele.cloudcover} </p>
               <p> lifted_index : {ele.lifted_index} </p>
               <p> seeing : {ele.seeing} </p>
---
+++ src/component/HookCounter2.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function HookCounter2() {
+
+     const initialCount = 0;
+     const [count, setCount] = useState(initialCount);
+
+     const Increment5 = () => {
+          for (let i = 0; i < 5; i++) {
+               setCount(prevCount => prevCount + 1);
+          }
+     }
+
+     return (
+          <div>
+               Count : {count}
+               <button onClick={() => setCount(initialCount)} > Reset </button>
+               <button onClick={prevCount => prevCount + 1} > Increment </button>
+               <button onClick={prevCount => prevCount - 1} > Decrement </button>
+               <button onClick={Increment5} > Increment 5 </button>
+          </div>
+     )
+}
+
+export default HookCounter2
